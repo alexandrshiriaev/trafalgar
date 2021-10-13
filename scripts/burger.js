9039f10a839bfb5d354ef15644f0360d9f8452a5
Alexandr Shiriaev <64530955+alexandrshiriaev@users.noreply.github.com>
2021-10-13 18:48:57 +0300

    Delete scripts directory

--- scripts/burger.js
+++
@@ -1,9 +0,0 @@
-const burgerBtn = document.querySelector('.burger');
-const navbar = document.querySelector('.navbar');
-const body = document.body;
-
-burgerBtn.addEventListener('click', () => {
-    navbar.classList.toggle('active');
-    burgerBtn.classList.toggle('active');
-    body.classList.toggle('overflow-hidden');
-})
